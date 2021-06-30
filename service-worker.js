de11f79f87aa7d19f0921edd2e5cb3bb42b032ac
Offor <offorb23@gmail.com>
2021-06-30 18:41:26 +0400

    final update

--- service-worker.js
+++ service-worker.js
@@ -15,6 +15,17 @@ var cacheFiles = [
 
 ];
 
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+
 self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (r) {
